0c76b4f1b99f20eae437ff0ee0f9ea0e71ce01a7
thearusoul7 <ar_kuanyshbai@kbtu.kz>
2026-03-01 14:03:56 +0500

    Delete aru/lab4 directory

--- aru/lab4/b5.py
+++
@@ -1,9 +0,0 @@
-#keri retpen shygaru
-def func(a):
-    for i in range(a, -1, -1):
-        yield i
-
-n = int(input())
-
-for b in func(n):
-    print(b)
--- aru/lab4/b7.py
+++
@@ -1,9 +0,0 @@
-#soz keri
-def func(a):
-    for i in a[::-1]:
-        yield i
-
-n = input()
-
-for b in func(n):
-    print(b, end="")
